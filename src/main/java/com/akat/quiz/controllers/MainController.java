e56cf80745a859de18751fc9bc937f561987d65d
Katyshev Aleksei <akatyshev@intech-global.com>
2019-09-28 21:04:56 +0300

    cros

--- src/main/java/com/akat/quiz/controllers/MainController.java
+++ src/main/java/com/akat/quiz/controllers/MainController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MainController {
 
     private final IMainService service;
